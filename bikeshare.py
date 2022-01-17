20d60ae17624d481771b6800472d63395ac2c75d
Chijioke Ibekwe <chijioke.ibekwe@sabiroad.com>
2022-01-17 06:33:05 +0100

    refactor: add delays in prompts

--- bikeshare.py
+++ bikeshare.py
@@ -16,11 +16,10 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     
     while True:
         try:
-            city = input("Kindly enter the city name (chicago, new york city, washington): ").lower()
+            city = input("\nKindly enter the city name (chicago, new york city, washington): ").lower()
             if city in ["chicago", "new york city", "washington"]:
                 break
             else:
@@ -28,7 +27,6 @@ def get_filters():
         except Exception:
             print("That's not a valid city!")
             
-    # TO DO: get user input for month (all, january, february, ... , june)
     while True:
         try:
             month = input("\nWhat month would you like to filter by (all, january, february, ... , june): ").lower()
@@ -39,7 +37,6 @@ def get_filters():
         except Exception:
             print("That's not a valid month!")
 
-    # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
     while True:
         try:
             day = input("\nWhat day would you like to filter by (all, monday, tuesday, ... sunday): ").lower()
@@ -65,28 +62,20 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
-    # load data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
 
-    # convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
-    # extract month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
-    df['day_of_week'] = df['Start Time'].dt.weekday_name
+    df['day_of_week'] = df['Start Time'].dt.day_name()
 
-    # filter by month if applicable
     if month != 'all':
-        # use the index of the months list to get the corresponding int
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(month) + 1
 
-        # filter by month to create the new dataframe
         df = df[df['month'] == month]
 
-    # filter by day of week if applicable
     if day != 'all':
-        # filter by day of week to create the new dataframe
         df = df[df['day_of_week'] == day.title()]
 
     return df
@@ -96,17 +85,16 @@ def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
+    time.sleep(3)
+
     start_time = time.time()
 
-    # TO DO: display the most common month
     m = df["month"].mode()[0] - 1
     months_of_year = ["January", "February", "March", "April", "May", "June"]
     print("Most common month of travel is {}".format(months_of_year[m]))
 
-    # TO DO: display the most common day of week
     print("Most common day of travel is {}".format(df["day_of_week"].mode()[0]))
 
-    # TO DO: display the most common start hour
     print("Most common start hour is {}".format(df["Start Time"].dt.hour.mode()[0]))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
@@ -117,15 +105,14 @@ def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
+    time.sleep(3)
+
     start_time = time.time()
 
-    # TO DO: display most commonly used start station
     print("Most commonly used start station is \"{}\"".format(df["Start Station"].mode()[0]))
 
-    # TO DO: display most commonly used end station
     print("Most commonly used end station is \"{}\"".format(df["End Station"].mode()[0]))
 
-    # TO DO: display most frequent combination of start station and end station trip
     df["station_combination"] = df["Start Station"] + "," + df["End Station"]
     first_station = df["station_combination"].mode()[0].split(",")[0]
     second_station = df["station_combination"].mode()[0].split(",")[1]
@@ -138,13 +125,13 @@ def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
+    time.sleep(3)
+
     start_time = time.time()
 
-    # TO DO: display total travel time
 
     print("Total travel time is {} minutes".format(df['Trip Duration'].sum().round()))
 
-    # TO DO: display mean travel time
     print("Mean travel time is {} minutes".format(df['Trip Duration'].mean().round()))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
@@ -155,20 +142,20 @@ def user_stats(df):
     """Displays statistics on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
+    time.sleep(3)
+    
     start_time = time.time()
 
-    # TO DO: Display counts of user types
     print("User Type Count is: ")
     print(df['User Type'].value_counts())
 
-    # TO DO: Display counts of gender
     print("\nGender Count details are as follows: ")
     
     try:
         print(df['Gender'].value_counts())
     except:
         print("Gender column is absent in the dataset")
-    # TO DO: Display earliest, most recent, and most common year of birth
+
     print("\nBirth Year stats are as follows: ")
     try:
         earliest_year = int(df['Birth Year'].min())
@@ -184,18 +171,18 @@ def user_stats(df):
 def display_raw_data(df):
     """ Prompts user to display raw data """
     i = 0
-    raw = input("Would you like to see the raw data (yes or no): ").lower() # TO DO: convert the user input to lower case using lower() function
+    raw = input("Would you like to see the raw data (yes or no): ").lower()
     pd.set_option('display.max_columns',200)
 
     while True:            
         if raw == 'no':
             break
         elif raw == 'yes':
-            print(df.iloc[i:i+5, :]) # TO DO: appropriately subset/slice your dataframe to display next five rows
-            raw = input("Would you like to see the next 5 rows (yes or no): ").lower() # TO DO: convert the user input to lower case using lower() function
+            print(df.iloc[i:i+5, :])
+            raw = input("Would you like to see the next 5 rows (yes or no): ").lower()
             i += 5
         else:
-            raw = input("\nYour input is invalid. Please enter only 'yes' or 'no'\n").lower()
+            raw = input("\nYour input is invalid. Please enter only 'yes' or 'no': ").lower()
 
 
 def main():
